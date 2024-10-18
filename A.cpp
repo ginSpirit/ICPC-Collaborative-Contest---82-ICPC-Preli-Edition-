d01f9b977aca6661bfb77b7c904f77084b4d2245
Md. Maksudur Rahman <maksudurrahman.avi7@gmail.com>
2024-10-18 21:20:19 +0600

    Create A.cpp

---
+++ A.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define input(v) for(auto &i : v) cin >> i
+#define endl "\n"
+#define all(x)  x.begin(), x.end()
+
+typedef long long ll;
+typedef long long int int64;
+const int INF = 0x3f3f3f3f;
+const ll LLINF = 0x3f3f3f3f3f3f3f3f;
+const long long MOD = 1e9+7;
+using namespace std;
+
+signed main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    
+    string s; cin >> s;
+    if (s.find('7') != string::npos) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+
+    return (EXIT_SUCCESS);
+}
